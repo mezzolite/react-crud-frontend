ed0b8387cbf942031edabfe4f02cb7828a7a8c20
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-11-07 18:21:14 -0700

    Coded DogCard component

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const DogCard = (props) => {
+
     return (
-        <h1>DogCard</h1>
+        <div className="dog-card">
+            <img src={props.dog.image} alt={props.dog.name} />
+            <div className="dog-specs">
+                <h4><b>{props.dog.name}</b></h4>
+                <p>{props.dog.breed}</p>
+            </div>
+        </div>
+
     )
 }
 
